ca962c58bdf1bae665b29bba4cfc29693af342f0
Bumblebee567 <maciekgasiorek9@gmail.com>
2018-06-10 16:43:06 +0200

    Choosing patient in making appointment

--- DentalSurgery/DentalSurgery/Utiles/AppointmentDropdownGenerator.cs
+++ DentalSurgery/DentalSurgery/Utiles/AppointmentDropdownGenerator.cs
@@ -16,6 +16,7 @@ namespace DentalSurgery.Utiles
             var model = new MakeAppointmentViewModel();
             model.Surgeries.AddRange(context.Set<Surgery>());
             model.Teeth.AddRange(context.Set<Tooth>());
+            model.Patients.AddRange(context.Set<AppUser>());
             foreach (var item in model.Surgeries)
             {
                 model.SurgeryChoice.Add(new SelectListItem { Text = item.Name, Value = item.SurgeryId.ToString() });
@@ -23,6 +24,10 @@ namespace DentalSurgery.Utiles
             foreach (var item in model.Teeth)
             {
                 model.ToothChoice.Add(new SelectListItem { Text = item.Name, Value = item.Id.ToString() });
+            }
+            foreach (var item in model.Patients)
+            {
+                model.PatientChoice.Add(new SelectListItem { Text = item.Email, Value = item.Id.ToString() });
             }
             return model;
         }
--- DentalSurgery/DentalSurgery/ViewModels/MakeAppointmentViewModel.cs
+++ DentalSurgery/DentalSurgery/ViewModels/MakeAppointmentViewModel.cs
@@ -16,8 +16,11 @@ namespace DentalSurgery.ViewModels
         public List<Guid> TeethIDs { get; set; } = new List<Guid>();
         public List<Surgery> Surgeries { get; set; } = new List<Surgery>();
         public List<Tooth> Teeth { get; set; } = new List<Tooth>();
+        public List<AppUser> Patients { get; set; } = new List<AppUser>();
         public List<SelectListItem> SurgeryChoice { get; set; } = new List<SelectListItem>();
         public List<SelectListItem> ToothChoice { get; set; } = new List<SelectListItem>();
+        public Guid PatientID { get; set; }
+        public List<SelectListItem> PatientChoice { get; set; } = new List<SelectListItem>();
         public int NumberOfSurgeries { get; set; }
     }
 }
